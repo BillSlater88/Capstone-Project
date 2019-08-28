6a0f232bf6bc08de699807569b9571fbdb709c8f
Bill Slater <william.slater527@gmail.com>
2019-08-27 19:17:43 -0500

    Initial Commit

--- src/main/java/org/launchcode/capstoneproject/controllers/RoundController.java
+++ src/main/java/org/launchcode/capstoneproject/controllers/RoundController.java
@@ -1,17 +1,25 @@
 package org.launchcode.capstoneproject.controllers;
 
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("round")
 public class RoundController {
 
 
+    @RequestMapping(value = "")
+    public String index(Model model) { return "/index"; }
 
 
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String dispalyAddRoundForm(Model model) {
+        return "/addround";
+    }
 
+    //@RequestMapping(value = "add", method = RequestMethod.POST)
+    //public String processAddRoundForm
 
 }
